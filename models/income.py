9bf37a3c8861bba61d51c490d22661ab5de3d0e1
Amara <amara.mailpro@gmail.com>
2025-04-11 18:31:39 +0200

    correction de l heritage class Income

--- models/income.py
+++ models/income.py
@@ -1,8 +1,8 @@
 from models.transaction import Transaction
 
 class Income(Transaction):
-    def __init__(self,date,origin, type):
-        super().__init__(date)
+    def __init__(self,montant, origin, date=None, description="") :
+        super().__init__(montant, date, description)
         self.origin = origin
-        self.type = type
+        self.type = "revenu"
 
